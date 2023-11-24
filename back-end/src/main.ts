37cd5ddfef37e7006de6f65b607843de6f9150dc
Vinicius <vini456nascimento@gmail.com>
2023-11-24 06:54:56 -0300

    feat: update main

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true }),
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { groups: ['transform'] },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Client connect')
+    .setDescription('List contact clients')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
